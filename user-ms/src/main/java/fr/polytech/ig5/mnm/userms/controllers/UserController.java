c79d72742e50ad4dbf8fb068a0da526d083c19cb
Nathan <ndmroot-git@yahoo.fr>
2022-10-28 19:25:17 +0200

    Initial commit

---
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/controllers/UserController.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.userms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from user ms!";
+    }
+}
