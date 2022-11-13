c012da1c91bd61c40ac8e49b68dc0dee9f0dbebc
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-11-13 14:37:01 +0100

    whip(container): docker init for user ms

--- user-ms/src/test/java/fr/polytech/ig5/mnm/userms/UserMsApplicationTests.java
+++ user-ms/src/test/java/fr/polytech/ig5/mnm/userms/UserMsApplicationTests.java
@@ -3,11 +3,16 @@ package fr.polytech.ig5.mnm.userms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class UserMsApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    void bite() {
+        assertEquals(true, true);
     }
+
 
 }
