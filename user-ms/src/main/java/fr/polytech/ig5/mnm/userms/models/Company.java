7551d97bd24a289135bafe22c7eb95447fcdfdcc
margotgeorget <margot.georget@etu.umontpellier.fr>
2022-11-15 16:12:43 +0100

    Created CRUD company

---
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/models/Company.java
@@ -0,0 +1,33 @@
+package fr.polytech.ig5.mnm.userms.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column()
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name="nb_employees")
+    private String nbEmployees;
+
+    @Column()
+    private String description;
+
+    @Column()
+    private SectorType sector;
+
+    @Column(name="picture_url")
+    private String pictureUrl;
+}
---
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/models/SectorType.java
@@ -0,0 +1,22 @@
+package fr.polytech.ig5.mnm.userms.models;
+
+public enum SectorType {
+    AGRICULTURE_PECHE,
+    ARTS,
+    BANUQE_ASSURANCE_IMMOBILIER,
+    COMMERCE_GRANDE_DISTRIBUTION,
+    COM_MEDIA,
+    CONSTRUCTION_BATIMENT_TRAVAUX_PUBLICS,
+    HOTELLERIE_RESTAURATION,
+    TOURISME_LOISIRS,
+    INDUSTRIE
+
+    /*
+    Installation et maintenance
+    Les métiers du secteur de la Santé
+    Services à la personne et à la collectivité
+    Spectacle
+    Support de l'entreprise
+    Transport et logistique
+     */
+}
