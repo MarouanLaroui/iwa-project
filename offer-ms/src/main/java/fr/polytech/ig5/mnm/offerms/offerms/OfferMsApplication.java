abdfb688dbed0ac0b91a89a6ca9c1972b565597a
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-11-15 17:00:08 +0100

    init(offer-ms): init of offer ms

---
+++ offer-ms/src/main/java/fr/polytech/ig5/mnm/offerms/offerms/OfferMsApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.offerms.offerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferMsApplication.class, args);
+	}
+
+}
---
+++ offer-ms/src/test/java/fr/polytech/ig5/mnm/offerms/offerms/OfferMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.offerms.offerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
