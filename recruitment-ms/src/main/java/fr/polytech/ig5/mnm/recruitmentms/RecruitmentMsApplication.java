5658ca2b7de03a62c7585bc672a12ff0b48a009a
margotgeorget <margot.georget@etu.umontpellier.fr>
2022-11-16 15:17:21 +0100

    init(recruitment-ms)

---
+++ recruitment-ms/src/main/java/fr/polytech/ig5/mnm/recruitmentms/RecruitmentMsApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.recruitmentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentMsApplication.class, args);
+	}
+
+}
---
+++ recruitment-ms/src/test/java/fr/polytech/ig5/mnm/recruitmentms/RecruitmentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.recruitmentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RecruitmentMsApplicationTests.class)
+class RecruitmentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
