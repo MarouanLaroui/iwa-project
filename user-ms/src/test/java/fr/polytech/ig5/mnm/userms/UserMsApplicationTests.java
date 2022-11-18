cc7ebed1fc3ee2a1feb04a74e86e641ebc398f57
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-11-18 11:17:00 +0100

    Update UserMsApplicationTests.java

--- user-ms/src/test/java/fr/polytech/ig5/mnm/userms/UserMsApplicationTests.java
+++ user-ms/src/test/java/fr/polytech/ig5/mnm/userms/UserMsApplicationTests.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = UserMsApplicationTests.class)
 class UserMsApplicationTests {
-
     @Test
     void isTrue() {
         assertEquals(true, true);
