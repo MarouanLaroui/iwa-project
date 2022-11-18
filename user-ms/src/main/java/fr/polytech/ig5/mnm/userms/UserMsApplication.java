474b098103638476af2c2a484f22fe67a51b1917
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-18 16:42:58 +0100

    WIP

--- user-ms/src/main/java/fr/polytech/ig5/mnm/userms/UserMsApplication.java
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/UserMsApplication.java
@@ -1,5 +1,6 @@
 package fr.polytech.ig5.mnm.userms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,9 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("fr.polytech.ig5.mnm.userms.repositories")
 @EntityScan("fr.polytech.ig5.mnm.userms.models")
 @SpringBootApplication
+@Slf4j
 public class UserMsApplication {
 
     public static void main(String[] args) {
+        log.info("***************** HERE ************************");
         SpringApplication.run(UserMsApplication.class, args);
     }
 }
--- user-ms/src/main/java/fr/polytech/ig5/mnm/userms/controllers/WorkerController.java
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/controllers/WorkerController.java
@@ -5,8 +5,10 @@ import fr.polytech.ig5.mnm.userms.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -23,6 +25,7 @@ public class WorkerController {
     }
 
     @GetMapping("/")
+    @RolesAllowed("worker")
     public List<Worker> index() {
         return this.service.findAll();
     }
