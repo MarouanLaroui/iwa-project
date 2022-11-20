abcf840a4a8091be01a4642360692c8ab17f9d69
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-11-20 08:36:00 +0100

    Update GatewayMsApplication.java

--- gateway-ms/src/main/java/fr/polytech/ig5/mnm/gatewayms/GatewayMsApplication.java
+++ gateway-ms/src/main/java/fr/polytech/ig5/mnm/gatewayms/GatewayMsApplication.java
@@ -33,7 +33,7 @@ public class GatewayMsApplication {
 						.and()
 						.uri(offerMsURI))
 
-				.route("r2", r -> r.path("/recruitment/**")
+				.route("r3", r -> r.path("/recruitment/**")
 						.and()
 						.uri(recruitmentMsURI))
 				.build();
