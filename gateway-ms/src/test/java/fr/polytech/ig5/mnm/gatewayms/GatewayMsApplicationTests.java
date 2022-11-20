cf541daf39b2a607974a5a6635ed7fa90138cc07
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-11-20 09:10:35 +0100

    config(docker): dockerisation

--- gateway-ms/src/test/java/fr/polytech/ig5/mnm/gatewayms/GatewayMsApplicationTests.java
+++ gateway-ms/src/test/java/fr/polytech/ig5/mnm/gatewayms/GatewayMsApplicationTests.java
@@ -3,7 +3,7 @@ package fr.polytech.ig5.mnm.gatewayms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatewayMsApplicationTests.class)
 class GatewayMsApplicationTests {
 
 	@Test
