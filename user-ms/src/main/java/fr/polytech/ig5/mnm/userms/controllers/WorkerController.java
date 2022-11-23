d099600c3df76b00404a60d632df1cb1ce0f1cd0
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-23 21:55:45 +0100

    fix(userms): address PR comments

--- user-ms/src/main/java/fr/polytech/ig5/mnm/userms/controllers/WorkerController.java
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/controllers/WorkerController.java
@@ -4,7 +4,6 @@ import fr.polytech.ig5.mnm.userms.DTO.WorkerCreateDTO;
 import fr.polytech.ig5.mnm.userms.DTO.WorkerUpdateDTO;
 import fr.polytech.ig5.mnm.userms.models.Worker;
 import fr.polytech.ig5.mnm.userms.services.WorkerService;
-import org.hibernate.jdbc.Work;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -55,12 +54,14 @@ public class WorkerController {
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody WorkerCreateDTO workerDTO) {
+        workerDTO.setPassword(workerDTO.getPassword());
         Worker worker = modelMapper.map(workerDTO, Worker.class);
         Worker workerCreated = service.create(worker);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(workerCreated);    }
+                .body(workerCreated);
+    }
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
