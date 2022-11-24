a3b8273190506f16b500e8ea710cb60166534a25
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-24 14:58:37 +0100

    fix(userms): company update dto fields

--- user-ms/src/main/java/fr/polytech/ig5/mnm/userms/DTO/CompanyUpdateDTO.java
+++ user-ms/src/main/java/fr/polytech/ig5/mnm/userms/DTO/CompanyUpdateDTO.java
@@ -5,22 +5,29 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class CompanyUpdateDTO {
 
+    @NotEmpty(message = "a company must have a name")
     private String name;
 
     @Email
+    @NotEmpty(message = "a company must have an email")
     private String email;
 
+    @NotEmpty(message = "a company must have a password")
     private String password;
 
     @Min(1)
+    @NotEmpty(message = "a company must have an employees number")
     private Integer employeesNumber;
 
+    @NotEmpty(message = "a company must have a description")
     private String description;
 
+    @NotEmpty(message = "a company must have a sector")
     private SectorType sector;
 
     private String pictureUrl;
