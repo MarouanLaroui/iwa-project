f77e3e0d4dadf00da50d5240d8a9f1c5ce75c9dd
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-29 18:12:14 +0100

    feat(feedbackms): initialize webflux

---
+++ feedback-ms/src/test/java/fr/polytech/ig5/mnm/feedbackms/feedbackms/FeedbackMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.feedbackms.feedbackms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
