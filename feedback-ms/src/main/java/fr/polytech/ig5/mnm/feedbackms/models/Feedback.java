0e867c8fe7ecc0d90c0a4934a450e1579aa6ac0a
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-30 13:23:46 +0100

    feat(feedbackms): add Margot's code

---
+++ feedback-ms/src/main/java/fr/polytech/ig5/mnm/feedbackms/models/Feedback.java
@@ -0,0 +1,36 @@
+package fr.polytech.ig5.mnm.feedbackms.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+
+@Data
+@Entity
+@Table(name="applications")
+public class Feedback {
+
+    @Id
+    @GeneratedValue()
+    private UUID feedbackId;
+
+    @Column(name="sender_id", nullable = false)
+    private UUID senderId;
+
+    @Column(name="receiver_id", nullable = false)
+    private UUID receiverId;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(columnDefinition="TEXT", nullable = false)
+    private String message;
+
+    @Column(name="job_label_rated", nullable = false)
+    private String jobLabelRated;
+
+    @Column(name="rate", nullable = false)
+    private int rate;
+
+}
