94a34dd5f5626a4d54974f4a7616eeb70e7f7289
margotgeorget <margot.georget@etu.umontpellier.fr>
2022-11-30 09:55:30 +0100

    init

---
+++ feedback-ms/src/main/java/fr/polytech/ig5/mnm/feedbackms/controllers/FeedbackController.java
@@ -0,0 +1,81 @@
+package fr.polytech.ig5.mnm.feedbackms.controllers;
+
+import fr.polytech.ig5.mnm.feedbackms.DTO.FeedbackCreateDTO;
+import fr.polytech.ig5.mnm.feedbackms.models.Feedback;
+import fr.polytech.ig5.mnm.feedbackms.services.FeedbackService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/feedbacks") // 1
+public class FeedbackController {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    FeedbackService service;
+
+    public FeedbackController(FeedbackService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/bySenderId/{id}")
+    public ResponseEntity<Object> getBySenderId(@PathVariable("id") UUID senderId) {
+
+        List<Feedback> feedbacks = this.service.findBySenderId(senderId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(feedbacks);
+    }
+
+    @GetMapping("/byReceiverId/{id}")
+    public ResponseEntity<Object> getByReceiverId(@PathVariable("id") UUID receiverId) {
+
+        List<Feedback> feedbacks = this.service.findByReceiverId(receiverId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(feedbacks);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(
+            @Valid @RequestBody FeedbackCreateDTO feedbackDTO) {
+
+        Feedback newFeedback = modelMapper.map(feedbackDTO, Feedback.class);
+        //newFeedback.setSenderId();
+
+        Feedback createdFeedback = this.service.create(newFeedback);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdFeedback);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> delete(@PathVariable("id") UUID id) {
+
+        var isRemoved = this.service.delete(id);
+
+        if (!isRemoved) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Feedback not found");
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(true);
+    }
+
+
+}
---
+++ feedback-ms/src/test/java/fr/polytech/ig5/mnm/feedbackms/FeedbackMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.ig5.mnm.feedbackms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FeedbackMsApplicationTests.class)
+class FeedbackMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
