c8c3baa28d52e1553d58b014060f285320575a35
NathanDjianMartin <ndmroot-git@yahoo.fr>
2022-11-30 09:08:53 +0100

    env(start-script): start docker dompose in background

--- start-prod.sh
+++ start-prod.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-cd kafka && docker compose up && cd ..
-cd gateway-ms && docker compose --env-file .env.dev up --build && cd ..
-cd user-ms && docker compose --env-file .env.dev up --build && cd ..
-cd offer-ms && docker compose --env-file .env.dev up --build && cd ..
-cd recruitment-ms && docker compose --env-file .env.dev up --build && cd ..
+cd kafka && docker compose up -d && cd ..
+cd gateway-ms && docker compose --env-file .env.dev up -d --build && cd ..
+cd user-ms && docker compose --env-file .env.dev up -d --build && cd ..
+cd offer-ms && docker compose --env-file .env.dev up -d --build && cd ..
+cd recruitment-ms && docker compose --env-file .env.dev up -d --build && cd ..
 # cd feedback-ms && docker compose --env-file .env.dev up --build && cd ..
 
